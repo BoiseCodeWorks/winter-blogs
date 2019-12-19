193de3d430688fadfd417e57069cd6e82640d610
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-12-19 11:37:04 -0700

    Wes

---
+++ src/components/Comments.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="comments">
+    <form @submit.prevent="createComment">
+      <input
+        v-model="comment"
+        type="text"
+        placeholder="Comment Here"
+        required
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Comments",
+  data() {
+    return {
+      comment: ""
+    };
+  },
+  methods: {
+    createComment() {
+      let comment = {
+        content: this.comment,
+        blogId: this.$route.params.id
+      };
+    }
+  }
+};
+</script>
+
+<style></style>
