639c379e9f03ccc5c613977af5cdf057ef943398
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-12-19 11:03:35 -0700

    styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,40 @@
   <div class="home container-fluid">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>Welcome to Your StarLogs App</h1>
-    <form @submit.prevent="createBlog">
-      <input
-        v-model="newBlog.title"
-        type="text"
-        placeholder="Blog Name"
-        required
-      />
-      <input
-        v-model="newBlog.body"
-        type="text"
-        placeholder="Description"
-        required
-      />
-      <input v-model="newBlog.img" type="text" placeholder="Optional Image" />
-      <button type="submit">Add Blog</button>
+    <form @submit.prevent="createBlog" class="d-flex">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input
+          v-model="newBlog.title"
+          type="text"
+          class="form-control"
+          placeholder="Blog Name"
+          id="title"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="desc">Description</label>
+        <input
+          class="form-control"
+          v-model="newBlog.body"
+          type="text"
+          placeholder="Description"
+          id="desc"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="img">Image Url</label>
+        <input
+          class="form-control"
+          id="img"
+          v-model="newBlog.img"
+          type="text"
+          placeholder="Optional Image"
+        />
+      </div>
+      <button class="btn btn-outline-success" type="submit">Add Blog</button>
     </form>
     {{ blogs }}
   </div>
