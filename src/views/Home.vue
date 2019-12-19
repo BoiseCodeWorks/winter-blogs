37b3a25e9b7ca4915718fb76b8e2997a8aacb12b
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-12-19 10:31:59 -0700

    gabe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+  <div class="home container-fluid">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Welcome to Your StarLogs App</h1>
+    <form @submit.prevent="createBlog">
+      <input
+        v-model="newBlog.title"
+        type="text"
+        placeholder="Blog Name"
+        required
+      />
+      <input
+        v-model="newBlog.body"
+        type="text"
+        placeholder="Description"
+        required
+      />
+      <input v-model="newBlog.img" type="text" placeholder="Optional Image" />
+      <button type="submit">Add Blog</button>
+    </form>
   </div>
 </template>
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      newBlog: {
+        title: "",
+        body: "",
+        img: ""
+      }
+    };
+  }
+};
+</script>
